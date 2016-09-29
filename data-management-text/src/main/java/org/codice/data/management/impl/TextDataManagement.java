8607906a3dc2de8d1acdaa7dc18c76cd62483803
bdeining <benjamin.deininger@connexta.com>
2016-09-28 19:31:25 -0600

    Added text package

---
+++ data-management-text/src/main/java/org/codice/data/management/impl/TextDataManagement.java
@@ -0,0 +1,36 @@
+package org.codice.data.management.impl;
+
+import org.codice.data.management.DataManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TextDataManagement implements DataManager {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TextDataManagement.class);
+
+    public TextDataManagement() {
+
+    }
+
+    public void init() {
+        LOGGER.trace("Initializing {}", TextDataManagement.class.getName());
+        System.out.println("ass");
+    }
+
+    @Override
+    public void insert() {
+        LOGGER.trace("Inserting Information");
+    }
+
+
+    @Override
+    public void update() {
+        LOGGER.trace("Updating Information");
+    }
+
+
+    @Override
+    public void delete() {
+        LOGGER.trace("Deleting Information");
+    }
+}
